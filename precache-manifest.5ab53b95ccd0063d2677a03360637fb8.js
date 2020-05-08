08e9bf253b9ad92f10d47bd7092ffdb59dcc8003
Apple <apple@MacBookPro.local>
2020-05-08 21:15:38 +0530

    Updates

---
+++ precache-manifest.5ab53b95ccd0063d2677a03360637fb8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f87c895bcb3550184a7c3768314f3661",
+    "url": "/reactsixteen/index.html"
+  },
+  {
+    "revision": "fb47577dab6b540db9bc",
+    "url": "/reactsixteen/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0878a7050dfa8d209037",
+    "url": "/reactsixteen/static/js/2.a62f966b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactsixteen/static/js/2.a62f966b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb47577dab6b540db9bc",
+    "url": "/reactsixteen/static/js/main.ad5bf2cd.chunk.js"
+  },
+  {
+    "revision": "27daa542abc6f5901ef6",
+    "url": "/reactsixteen/static/js/runtime-main.571ddcb2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactsixteen/static/media/logo.5d5d9eef.svg"
+  }
+]);
